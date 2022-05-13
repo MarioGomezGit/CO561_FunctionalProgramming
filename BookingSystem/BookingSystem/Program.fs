70fabb7fb0f13bc6efcf698b185c42efba589484
Jose Gomes <72214603+MarioGomezGit@users.noreply.github.com>
2022-05-13 05:33:06 +0100

    Add files via upload

---
+++ BookingSystem/BookingSystem/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+//Task 4
+type Ticket = {seat:int; customer:string}
+let mutable tickets = [for n in 1..10 -> {Ticket.seat = n; Ticket.customer = ""}]
+
+let BookSeat =
+    Console.WriteLine("Enter seat number:")
+    let seatNumber = int(Console.ReadLine())
+    Console.WriteLine("Enter customer name:")
+    let customerName = Console.ReadLine()
+    Console.Write(seatNumber) //so da merda para baixo
+    let book seatNumber customerName tickets = 
+        tickets |> List.map (fun ticket ->
+            if ticket.seat = seatNumber then { ticket with customer = customerName }
+            else ticket )    
+    tickets <- book seatNumber customerName tickets
+
+let DisplayTickets = Console.WriteLine(tickets)
